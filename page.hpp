51581295a6514fafc64b85b3299ae4c70f7b944e
RonistoneJunior <ronistonejunior@gmail.com>
2017-11-27 09:08:00 -0200

    HeapFile Complete

---
+++ page.hpp
@@ -0,0 +1,66 @@
+#ifndef __PAGE__
+#define __PAGE__
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PAGE_SIZE 64000
+#define REG_N (size_t)(PAGE_SIZE/sizeof(T))
+#include "standard_reg.hpp"
+
+template<typename T> class page{
+  private:
+    T reg[REG_N];
+
+  public:
+    int insert(T data){
+      for(int i=0;i<REG_N;i++){
+        if(reg[i].empty()){
+          reg[i] = data;
+          return i;
+        }
+      }
+      return -1;
+    }
+
+    void remove(T data){
+      for(int i=0;i<REG_N;i++){
+        if(data == reg[i]){
+          reg[i].clear();
+        }
+      }
+    }
+    void remove(int slotId){
+      if(slotId < REG_N and !reg[slotId].empty()){
+        reg[slotId].clear();
+      }
+    }
+    void update(int slotId, T data){
+      if(slotId < REG_N and slotId >=0){
+        reg[slotId] = data;
+      }
+    }
+
+    void scan(){
+      for(int i=0;i<REG_N;i++){
+        if(!reg[i].empty()){
+            reg[i].scan();
+        }
+      }
+    }
+
+    bool empty(){
+      for(int i=0;i<REG_N;i++){
+        if(!reg[i].empty())
+          return false;
+      }
+      return true;
+    }
+    size_t slotsPage(){
+      return REG_N;
+    }
+    size_t sizePage(){
+      return PAGE_SIZE;
+    }
+};
+
+#endif
---
+++ standard_reg.hpp
@@ -0,0 +1,63 @@
+#ifndef _STANDARD_REG_
+#define _STANDARD_REG_
+
+#include <bits/stdc++.h>
+using namespace std;
+
+template<size_t T> struct standard_reg{
+  private:
+      int a;
+      unsigned short int size;
+      char b[T];
+      int c;
+
+  public:
+      standard_reg();
+      standard_reg(int _a, char _b[], int _c);
+
+      bool operator==(standard_reg b);
+
+      void clear();
+      bool empty();
+      void scan();
+};
+
+template<size_t T>
+standard_reg<T>::standard_reg(){
+  this->clear();
+}
+template<size_t T>
+standard_reg<T>::standard_reg(int _a, char _b[], int _c){
+  a = _a;
+  strcpy(b,_b);
+  size = T;
+  c = _c;
+}
+
+template<size_t T>
+bool standard_reg<T>::operator==(standard_reg<T> b){
+  return (this->a == b.a) and (this->c == b.c) and (this->size == b.size) and (!strcmp(this->b, b.b));
+}
+
+template<size_t T>
+void standard_reg<T>::clear(){
+  a = INT_MIN;
+  size = USHRT_MAX;
+  b[0] = '\0';
+  c = INT_MIN;
+}
+
+template<size_t T>
+bool standard_reg<T>::empty(){
+  return ((this->a==INT_MIN) and (this->size == USHRT_MAX) and (this->b[0] == '\0') and (this->c == INT_MIN));
+}
+
+template<size_t T>
+void standard_reg<T>::scan(){
+  cout << "A: " << a << endl
+       << "Size: " << size << endl
+       << "B: " << b << endl
+       << "C: " << c << endl;
+}
+
+#endif
